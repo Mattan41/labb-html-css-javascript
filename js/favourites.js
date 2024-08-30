43856d4494b2b9474d5b3eb16fabc49060a9adfb
Mats Kruskopf Eriksson <mats.kruskopf.eriksson@iths.se>
2024-08-30 12:09:13 +0200

    update

--- js/favourites.js
+++ js/favourites.js
@@ -1,11 +1,15 @@
-/*
-document.querySelector('i.fa-solid.fa-f').addEventListener('click', function () {
-    console.log('Klickad!');
-    document.querySelector('#favourites-container').classList.toggle('d-none');
-});
-*/
+import {loadFavouriteList, updateFavouriteList} from './global.js';
 
 document.querySelector('i.fa-solid.fa-f').addEventListener('click', () => {
     const favoritesContainer = document.querySelector('#favourites-container');
     favoritesContainer.classList.toggle('show'); // Använd en egen klass för att visa/dölja
+    loadFavouriteList();
+});
+
+
+
+document.querySelector('#close-favourites').addEventListener('click', () => {
+    const favoritesContainer = document.querySelector('#favourites-container');
+    favoritesContainer.classList.remove('show');
+    updateFavouriteList(); // Update the favorite list when the favorites container is closed
 });
--- js/global.js
+++ js/global.js
@@ -1,14 +1,58 @@
-const favoriteMovies = [];
+let movieDataMap;
 
-function updateFavouriteList(id) {
-    const favoriteListElement = document.querySelector('#favorite-list');
-    favoriteListElement.innerHTML = '';
-    const movieData = movieDataMap.get(id);
-    if (movieData && movieData.favourite) {
-        const movieElement = document.createElement('li');
-        movieElement.textContent = movieData.title;
-        favoriteListElement.appendChild(movieElement);
-    }
+export function fetchMovies() {
+    return fetch('../labb1/json/data.json', { mode: 'no-cors' })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            movieDataMap = new Map(data.map(movie => [movie.id, movie]));
+            loadFavouriteList(); // Load favourites from local storage
+            return movieDataMap;
+        })
+        .catch(error => console.error(error));
 }
 
-//todo add more features (such as persisting the favorite list across page reloads, or displaying the favorite list in a separate page)
+// todo: call on function to update the favourite list
+export function setStarIconColor(starIcon, isFavorite) {
+    starIcon.style.color = isFavorite ? 'yellow' : 'black';
+}
+
+export function updateFavouriteList() {
+
+    const favoriteMovies = Array.from(movieDataMap.values()).filter(movie => movie.favourite);
+    localStorage.setItem('favoriteMovies', JSON.stringify(favoriteMovies));
+    displayStoredMovieList(favoriteMovies);
+
+}
+
+export function loadFavouriteList() {
+    const favoriteMovies = JSON.parse(localStorage.getItem('favoriteMovies')) || [];
+    favoriteMovies.forEach(favMovie => {
+        if (movieDataMap.has(favMovie.id)) {
+            movieDataMap.get(favMovie.id).favourite = true;
+        }
+    });
+    displayStoredMovieList(favoriteMovies);
+}
+
+function displayStoredMovieList(favoriteMovies) {
+    const favoriteListElement = document.querySelector('#favorite-list');
+    favoriteListElement.innerHTML = '';
+    favoriteMovies.forEach(movie => {
+        const movieElement = document.createElement('li');
+        const starIcon = document.createElement('i');
+        starIcon.classList.add('fas', 'fa-star');
+        starIcon.title = 'Remove from favourites';
+        starIcon.addEventListener('click', () => {
+            movie.favourite = false;
+            updateFavouriteList();
+        });
+        movieElement.appendChild(starIcon);
+        movieElement.appendChild(document.createTextNode(movie.title));
+        favoriteListElement.appendChild(movieElement);
+    });
+}
