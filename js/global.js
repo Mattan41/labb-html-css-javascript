4a9649ba12dc3742ec66f65c9c759dc73fe61115
Mats Kruskopf Eriksson <143085004+Mattan41@users.noreply.github.com>
2024-10-07 10:24:54 +0200

    update with accesstoken in env

--- js/global.js
+++ js/global.js
@@ -1,9 +1,11 @@
 import {handleUrlChange} from "./movie-details.js";
 import {initializeLogin} from "./login.js";
 
+// const API_KEY = import.meta.env.VITE_API_KEY;
+// const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
+const API_URL = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`;
 
-//const API_KEY = todo: add your API key here as a environment variable
-const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 let movieDataMap;
 
 window.onload = function() {
@@ -20,34 +22,75 @@ export function fetchMovies() {
         loadFavouriteList();
         return Promise.resolve(movieDataMap);
     } else {
-        return fetch(API_URL)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                }
-                return response.json();
-            })
-            .then(data => {
-                const movies = data.results.map(movie => ({
-                    id: movie.id,
-                    title: movie.title,
-                    genre: movie.genre_ids.map(id => mapGenreIdToName(id)), // Assuming you have a function to map genre IDs to names
-                    year: new Date(movie.release_date).getFullYear(),
-                    rating: movie.vote_average,
-                    director: [], // Todo remove and an extra API call for displaying movie details
-                    actors: [], // Todo search movies by title, update the movie object with results from the search
-                    synopsis: movie.overview,
-                    imageUrl: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-                    favourite: false
-                }));
-                localStorage.setItem('movieData', JSON.stringify(movies));
-                movieDataMap = new Map(movies.map(movie => [movie.id, movie]));
-                loadFavouriteList();
-                return movieDataMap;
-            })
-            .catch(error => console.error(error));
+        return fetch(API_URL, {
+            headers: {
+                'Authorization': `Bearer ${ACCESS_TOKEN}`
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const movies = data.results.map(movie => ({
+                id: movie.id,
+                title: movie.title,
+                genre: movie.genre_ids.map(id => mapGenreIdToName(id)), // Assuming you have a function to map genre IDs to names
+                year: new Date(movie.release_date).getFullYear(),
+                rating: movie.vote_average,
+                director: [], // Todo remove and an extra API call for displaying movie details
+                actors: [], // Todo search movies by title, update the movie object with results from the search
+                synopsis: movie.overview,
+                imageUrl: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+                favourite: false
+            }));
+            localStorage.setItem('movieData', JSON.stringify(movies));
+            movieDataMap = new Map(movies.map(movie => [movie.id, movie]));
+            loadFavouriteList();
+            return movieDataMap;
+        })
+        .catch(error => console.error(error));
     }
 }
+// export function fetchMovies() {
+//     const localData = localStorage.getItem('movieData');
+//     if (localData) {
+//         movieDataMap = new Map(JSON.parse(localData).map(movie => [movie.id, movie]));
+//         loadFavouriteList();
+//         return Promise.resolve(movieDataMap);
+//     } else {
+//         return fetch(API_URL)
+//             .then(response => {
+//                 if (!response.ok) {
+//                     throw new Error('Network response was not ok ' + response.statusText);
+//                 }
+//                 return response.json();
+//             })
+//             .then(data => {
+//                 const movies = data.results.map(movie => ({
+//                     id: movie.id,
+//                     title: movie.title,
+//                     genre: movie.genre_ids.map(id => mapGenreIdToName(id)), // Assuming you have a function to map genre IDs to names
+//                     year: new Date(movie.release_date).getFullYear(),
+//                     rating: movie.vote_average,
+//                     director: [], // Todo remove and an extra API call for displaying movie details
+//                     actors: [], // Todo search movies by title, update the movie object with results from the search
+//                     synopsis: movie.overview,
+//                     imageUrl: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+//                     favourite: false
+//                 }));
+//                 localStorage.setItem('movieData', JSON.stringify(movies));
+//                 movieDataMap = new Map(movies.map(movie => [movie.id, movie]));
+//                 loadFavouriteList();
+//                 return movieDataMap;
+//             })
+//             .catch(error => console.error(error));
+//     }
+// }
+
+
 function mapGenreIdToName(id) {
     const genres = {
         28: 'Action',
