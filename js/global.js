7ca8ea88c7b3d0952622ef111a433216b2ec8851
Mats Kruskopf Eriksson <143085004+Mattan41@users.noreply.github.com>
2024-10-08 08:55:09 +0200

    update webpack

--- js/global.js
+++ js/global.js
@@ -1,7 +1,7 @@
 import {handleUrlChange} from "./movie-details.js";
 import {initializeLogin} from "./login.js";
 
-const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
+const ACCESS_TOKEN = import.meta.env.ACCESS_TOKEN;
 const API_URL = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`;
 
 let movieDataMap;
